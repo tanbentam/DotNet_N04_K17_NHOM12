776b8bd970d0f7ef1a25853b0ac32a54d13cda1f
Chu Thanh Tan <chuthanhtan1995@gmail.com>
2026-06-02 15:03:30 +0700

    Render root views directly in main shell

--- TravelApp.Frontend/ViewModels/MainViewModel.cs
+++ TravelApp.Frontend/ViewModels/MainViewModel.cs
@@ -1,6 +1,15 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls;
 using TravelApp.Frontend.Contracts;
 using TravelApp.Frontend.Services.NotificationQueue;
+using TravelApp.Frontend.ViewModels.Admin;
+using TravelApp.Frontend.ViewModels.Authentication;
+using TravelApp.Frontend.ViewModels.TourGuide;
+using TravelApp.Frontend.ViewModels.User;
+using TravelApp.Frontend.Views.Admin;
+using TravelApp.Frontend.Views.Authentication;
+using TravelApp.Frontend.Views.TourGuide;
+using TravelApp.Frontend.Views.User;
 
 namespace TravelApp.Frontend.ViewModels
 {
@@ -8,6 +17,7 @@ namespace TravelApp.Frontend.ViewModels
     {
         private readonly INavigationService _navigationService;
         private ObservableObject _currentViewModel;
+        private UserControl _currentView;
         private string _windowTitle = "Digital Travel Application";
 
         public NotificationManager NotificationManager { get; }
@@ -21,7 +31,19 @@ namespace TravelApp.Frontend.ViewModels
         public ObservableObject CurrentViewModel
         {
             get => _currentViewModel;
-            set => SetProperty(ref _currentViewModel, value);
+            set
+            {
+                if (SetProperty(ref _currentViewModel, value))
+                {
+                    CurrentView = BuildRootView(value);
+                }
+            }
+        }
+
+        public UserControl CurrentView
+        {
+            get => _currentView;
+            set => SetProperty(ref _currentView, value);
         }
 
         public MainViewModel(INavigationService navigationService, NotificationManager notificationManager)
@@ -30,6 +52,39 @@ namespace TravelApp.Frontend.ViewModels
             NotificationManager = notificationManager;
             _navigationService.CurrentViewModelChanged += viewModel => CurrentViewModel = viewModel;
             _navigationService.NavigateToLogin();
+        }
+
+        private static UserControl BuildRootView(ObservableObject viewModel)
+        {
+            UserControl view;
+
+            if (viewModel is LoginViewModel)
+            {
+                view = new LoginView();
+            }
+            else if (viewModel is RegisterViewModel)
+            {
+                view = new RegisterView();
+            }
+            else if (viewModel is AdminDashboardViewModel)
+            {
+                view = new AdminDashboardView();
+            }
+            else if (viewModel is GuideDashboardViewModel)
+            {
+                view = new GuideDashboardView();
+            }
+            else if (viewModel is UserDashboardViewModel)
+            {
+                view = new UserDashboardView();
+            }
+            else
+            {
+                view = new UserControl();
+            }
+
+            view.DataContext = viewModel;
+            return view;
         }
     }
 }
