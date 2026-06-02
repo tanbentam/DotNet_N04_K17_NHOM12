d613102036617f8ddb7ed94bb5879a45c752f213
Chu Thanh Tan <chuthanhtan1995@gmail.com>
2026-06-02 13:52:02 +0700

    Update cau truc thu muc moi

---
+++ TravelApp.Backend/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TravelApp.Backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TravelApp Backend API");
+        }
+    }
+}
---
+++ TravelApp.Backend/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TravelApp.Backend")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TravelApp.Backend")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ab71ca8-84db-426b-9878-c182fee7fce3")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
